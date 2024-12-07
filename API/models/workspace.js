831dc703ea8888a2d6bd432498b95172b14ed4cf
Hiruna15 <hirunadilmith15@gmail.com>
2024-12-07 11:27:49 +0530

    update workspace schema

--- API/models/workspace.js
+++ API/models/workspace.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const workspaceSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Workspace name is required"],
+const workspaceSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Workspace name is required"],
+    },
+    description: String,
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    projects: [{ type: mongoose.Schema.Types.ObjectId, ref: "Project" }],
   },
-  description: String,
-});
+  { timestamps: true }
+);
 
-const workspaceModel = mongoose.model("Workspace", workspaceSchema);
+const WorkspaceModel = mongoose.model("Workspace", workspaceSchema);
 
-export default workspaceModel;
+export default WorkspaceModel;
