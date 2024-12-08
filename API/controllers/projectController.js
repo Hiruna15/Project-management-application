976c35b8a073b29e03d57b3a95294a16e09ae0a3
Hiruna15 <hirunadilmith15@gmail.com>
2024-12-08 23:47:46 +0530

    create project controllers

---
+++ API/controllers/projectController.js
@@ -0,0 +1,70 @@
+import "express-async-errors";
+import mongoose from "mongoose";
+import AppError from "../errors/AppError.js";
+import ProjectModel from "../models/project.js";
+
+const createProject = async (req, res, next) => {
+  if (Object.keys(req.body).length === 0) {
+    return next(new AppError("No data sent in the request body", 400));
+  }
+
+  const newProject = await ProjectModel.create(req.body);
+  res.status(200).json({ data: newProject });
+};
+
+const getProjectsInaWorkspace = async (req, res, next) => {
+  const workspaceId = req.params.workspaceId;
+
+  if (!mongoose.isValidObjectId(workspaceId)) {
+    return next(new AppError("Invalid workspaceId provided", 400));
+  }
+
+  const projects = await ProjectModel.find({ workspace: workspaceId });
+
+  if (projects.length === 0) {
+    return next(
+      new AppError("No projects found for the given workspaceId", 404)
+    );
+  }
+
+  res.status(200).json({ data: projects });
+};
+
+const updateProject = async (req, res, next) => {
+  const data = req.body;
+  const { workspaceId, projectId } = req.params;
+
+  if (Object.keys(data).length === 0) {
+    return next(new AppError("No data sent in the request body", 400));
+  }
+
+  if (!workspaceId || !projectId) {
+    return next(new AppError("workspaceId and projectId are required", 400));
+  }
+
+  if (
+    !mongoose.isValidObjectId(workspaceId) ||
+    !mongoose.isValidObjectId(projectId)
+  ) {
+    return next(new AppError("Invalid workspaceId or projectId provided", 400));
+  }
+
+  const updatedProject = await ProjectModel.findByIdAndUpdate(
+    { workspace: workspaceId, _id: projectId },
+    data,
+    { runValidators: true, new: true }
+  );
+
+  if (!updatedProject) {
+    return next(
+      new AppError(
+        "No projects found for the given workspaceId and projectId",
+        404
+      )
+    );
+  }
+
+  res.status(200).json({ data: updatedProject });
+};
+
+export { createProject, getProjectsInaWorkspace, updateProject };
--- API/routes/projectRoutes.js
+++ API/routes/projectRoutes.js
@@ -1,85 +1,16 @@
 import express from "express";
-import ProjectModel from "../models/project.js";
-import "express-async-errors";
-import AppError from "../errors/AppError.js";
-import mongoose from "mongoose";
+import {
+  createProject,
+  getProjectsInaWorkspace,
+  updateProject,
+} from "../controllers/projectController.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
-  if (Object.keys(req.body).length === 0) {
-    return next(new AppError("No data is sent in the request body", 401));
-  }
+router.post("/", createProject);
 
-  const newProject = await ProjectModel.create(req.body);
-  res.status(200).json({ data: newProject });
-});
+router.get("/:workspaceId", getProjectsInaWorkspace);
 
-router.get("/:workspaceId", async (req, res, next) => {
-  const workspaceId = req.params.workspaceId;
-
-  if (!mongoose.isValidObjectId(workspaceId)) {
-    return next(new AppError("Invalid workspaceId provided", 400));
-  }
-
-  const projects = await ProjectModel.find({ workspace: workspaceId });
-
-  if (projects.length === 0) {
-    return next(
-      new AppError("No projects found for the given workspaceId", 404)
-    );
-  }
-
-  res.status(200).json({ data: projects });
-});
-
-router.patch("/update/:workspaceId/:projectId", async (req, res, next) => {
-  const data = req.body;
-  const { workspaceId, projectId } = req.params;
-
-  if (Object.keys(data).length === 0) {
-    return next(new AppError("No data is sent in the request body", 401));
-  }
-
-  if (!workspaceId || !projectId) {
-    return next(
-      new AppError("some data is missing in the request params", 401)
-    );
-  }
-
-  if (
-    !mongoose.isValidObjectId(workspaceId) ||
-    !mongoose.isValidObjectId(projectId)
-  ) {
-    return next(new AppError("Invalid workspaceId or projectId provided", 400));
-  }
-
-  const project = await ProjectModel.findOne({
-    workspace: workspaceId,
-    _id: projectId,
-  });
-
-  if (Object.keys(project).length === 0) {
-    return next(
-      new AppError(
-        "No projects found for the given workspace and projectid",
-        404
-      )
-    );
-  }
-
-  const updatedProject = await ProjectModel.findByIdAndUpdate(
-    project._id,
-    data,
-    { runValidators: true, new: true }
-  );
-
-  res.status(200).json({ data: updatedProject });
-});
-
-router.get("/", async (req, res) => {
-  const projects = await ProjectModel.find({});
-  res.json({ projects });
-});
+router.patch("/update/:workspaceId/:projectId", updateProject);
 
 export default router;
